5cf3e3c5ca4f2a58459ed7d012f83c1c6ce3dc4f
hayatSalawdeh <salawdeh.hayat@gmail.com>
2022-10-21 03:57:48 +0300

    done

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header({onDarkModeClick, isDarkMode}){
+    return (
+    <header>
+        <h2>Shopster</h2>
+        <button onClick={onDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+      </header>);
+}
